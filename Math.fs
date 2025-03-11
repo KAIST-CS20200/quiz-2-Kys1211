9f1d0efc578deb3e8dbdecf77d0ef09bcef16e88
YoungsooKang <mongkeydejhv@kaist.ac.kr>
2025-03-11 13:26:32 +0900

    Passed all tests

--- Math.fs
+++ Math.fs
@@ -5,5 +5,13 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
+let f (n : int64) =
+  if n % 2L = 0L then n / 2L
+  else 3L * n + 1L
+
 let collatz n =
-  0
+  let rec iter cnt (n : int64) = 
+    let result = f n
+    if result = 1L then cnt
+    else iter (cnt + 1) result
+  iter 1 n
